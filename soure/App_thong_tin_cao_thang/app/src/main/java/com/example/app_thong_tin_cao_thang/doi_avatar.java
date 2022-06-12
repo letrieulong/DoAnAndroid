81b66ae1f20051f0263e1f0f80711f01ccfb4424
duykhang259 <85671645+duykhang259@users.noreply.github.com>
2022-06-12 08:45:07 +0700

    giao dien login

---
+++ soure/App_thong_tin_cao_thang/app/src/main/java/com/example/app_thong_tin_cao_thang/doi_avatar.java
@@ -0,0 +1,14 @@
+package com.example.app_thong_tin_cao_thang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class doi_avatar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doi_avatar);
+    }
+}
---
+++ soure/App_thong_tin_cao_thang/app/src/main/java/com/example/app_thong_tin_cao_thang/trang_chu.java
@@ -0,0 +1,16 @@
+package com.example.app_thong_tin_cao_thang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class trang_chu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trang_chu);
+    }
+}
