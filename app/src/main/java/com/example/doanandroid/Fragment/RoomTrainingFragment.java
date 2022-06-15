534297d215b22ef126a3b2673ca8f8f7933397f7
letrieulong <letrieulong412000@gmail.com>
2022-06-15 17:30:50 +0700

    code

--- app/src/main/java/com/example/doanandroid/Fragment/RoomTrainingFragment.java
+++ app/src/main/java/com/example/doanandroid/Fragment/RoomTrainingFragment.java
@@ -2,6 +2,7 @@ package com.example.doanandroid.Fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -10,12 +11,21 @@ import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.ViewFlipper;
 
 import com.bumptech.glide.Glide;
+import com.example.doanandroid.Model.ContactMechanical;
+import com.example.doanandroid.Model.Mechanical;
 import com.example.doanandroid.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class RoomTrainingFragment extends Fragment {
 
@@ -38,6 +48,100 @@ public class RoomTrainingFragment extends Fragment {
     private void init() {
         viewFlipper = view.findViewById(R.id.viewflipper);
     }
+
+    // get data
+//    private void getDataFireBase() {
+//        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference("list_Mechanical");
+//        // get data từ firebase
+//        mDatabase.child("list_recruit").addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+////                mechanicalList.clear();
+//                List<String> list = new ArrayList<>();
+//                for (DataSnapshot dt : snapshot.getChildren()) {
+//                    list.add(dt.getKey());
+//                    Mechanical rs = dt.getValue(Mechanical.class);
+////                    mechanicalList.add(rs);
+//                }
+////                adapterRecruit_mechanical.notifyDataSetChanged();
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        });
+//
+//        // contact
+//        mDatabase.child("contact").addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                ContactMechanical ct = snapshot.getValue(ContactMechanical.class);
+//                setText(view.findViewById(R.id.txt_email), ct.getEmail());
+//                setText(view.findViewById(R.id.txt_phone), ct.getPhone());
+//                setText(view.findViewById(R.id.txt_address), ct.getAddress());
+//
+//                // truy cập vào từng phần tử
+////                HashMap<String,Object> hashMap= (HashMap<String, Object>) snapshot.getValue();
+//                //HashMap này sẽ có kích  thước bằng số Node con bên trong node truy vấn
+//                //mỗi phần tử có key là name được định nghĩa trong cấu trúc Json của Firebase
+////                Log.d("abc",hashMap.get("email").toString());
+//                adapterRecruit_mechanical.notifyDataSetChanged();
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        });
+//
+//        // access
+//        mDatabase.child("access").addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//
+//                List<String> access = new ArrayList<>();
+//                for (DataSnapshot snap : snapshot.getChildren()) {
+//                    access.add(snap.getValue().toString());
+//                }
+//                setText(view.findViewById(R.id.txt_access_today), "Hôm nay : " + access.get(1));
+//                setText(view.findViewById(R.id.access_month), "Tháng này : " + access.get(0));
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        });
+//
+//        // lịch tiếp sinh viên
+//        mDatabase.child("list_notification").addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                mechanicalList_noti.clear();
+//                List<String> list = new ArrayList<>();
+//                for (DataSnapshot dt : snapshot.getChildren()) {
+//                    list.add(dt.getKey());
+//                    Mechanical rs = dt.getValue(Mechanical.class);
+//                    mechanicalList_noti.add(rs);
+//                }
+//                adapterNotification_mechanical.notifyDataSetChanged();
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        });
+//    }
+
+    //Hỗ trợ đổi TEXT
+    private void setText(final TextView text, final String value) {
+        if (text != null) {
+            text.setText(value);
+        }
+    }
+
 
     // banner
     private void Acviewflipper() {
