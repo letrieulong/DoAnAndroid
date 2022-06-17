bc2a2f7a87090d3b092ee247aaa0959f01afa5ee
LeVinhDuy <leduyltp221@gmail.com>
2022-06-17 14:54:45 +0700

    icrease like

--- app/src/main/java/com/example/doanandroid/DetailPostActivity.java
+++ app/src/main/java/com/example/doanandroid/DetailPostActivity.java
@@ -7,10 +7,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import com.example.doanandroid.Model.Infor_All_CNTT;
 import com.example.doanandroid.Model.Recruit_CNTT;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -27,7 +31,9 @@ public class DetailPostActivity extends AppCompatActivity {
     TextView tvTitle, tvContent, tvLike, tvCreate, tvView;
     Recruit_CNTT recruit_cntt;
     String postID;
-
+    FloatingActionButton floatingActionButton;
+    boolean isLike = true;
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,8 +43,71 @@ public class DetailPostActivity extends AppCompatActivity {
         recruit_cntt = (Recruit_CNTT) getIntent().getSerializableExtra("recruit");
         setUpViews();
         getdata();
+        initEvents();
 
 
+    }
+
+    private void initEvents() {
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isLike){
+                    increaseLikeInPostDeail();
+                    saveLikeInLikesDB();
+                }else{
+                    reduceLikeInPostDetail();
+                    deleteLikeInLikesDB();
+                }
+
+            }
+        });
+    }
+
+    private void deleteLikeInLikesDB() {
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference("List_like");
+        mDatabase.child(user.getUid()).child(postID).removeValue();
+        floatingActionButton.setImageDrawable(getResources().getDrawable(R.drawable.heart));
+        isLike = true;
+    }
+
+    private void reduceLikeInPostDetail() {
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference("list_cntt");
+        mDatabase.child("recruit").child(postID).child("like").setValue((recruit_cntt.getLike() - 1));
+    }
+
+    void checkLike(){
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference("List_like");
+        mDatabase.child(user.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot childSnapshot: dataSnapshot.getChildren()) {
+                    String id = childSnapshot.getKey().toString();
+                    if (id.equals(postID)){
+                        isLike = false;
+                        floatingActionButton.setImageDrawable(getResources().getDrawable(R.drawable.brokenheart));
+                        break;
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.d("asd", databaseError.toString());
+            }
+        });
+    }
+
+    private void saveLikeInLikesDB() {
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference("List_like");
+        mDatabase.child(user.getUid()).child(postID).setValue("");
+
+    }
+
+    private void increaseLikeInPostDeail() {
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference("list_cntt");
+        mDatabase.child("recruit").child(postID).child("like").setValue((recruit_cntt.getLike() +1));
+        floatingActionButton.setImageDrawable(getResources().getDrawable(R.drawable.brokenheart));
+        isLike = false;
     }
 
     private void getdata(){
@@ -50,6 +119,7 @@ public class DetailPostActivity extends AppCompatActivity {
                     postID = childSnapshot.getKey().toString();
                     getDataByPostID(postID);
                     increaseView();
+                    checkLike();
                 }
             }
             @Override
@@ -96,6 +166,6 @@ public class DetailPostActivity extends AppCompatActivity {
         tvView = findViewById(R.id.detail_post);
         tvLike = findViewById(R.id.detail_post_like);
         tvCreate = findViewById(R.id.detail_post_date_create);
-
+        floatingActionButton = findViewById(R.id.fab_detail_post);
     }
 }
